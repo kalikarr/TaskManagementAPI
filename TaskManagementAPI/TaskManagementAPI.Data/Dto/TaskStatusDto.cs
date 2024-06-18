6eb27e9c904e1b248f5da3623ad6cc11a1989987
kalika <kalika.ranasinghe@1billiontech.com>
2024-06-18 07:45:31 +0530

    Rename Item

---
+++ TaskManagementAPI/TaskManagementAPI.Data/Dto/TaskStatusDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementAPI.Data.Dto
+{
+    public class TaskStatusDto
+    {
+        public int StatusId { get; set; }
+        public string StatusName { get; set; }
+    }
+}
